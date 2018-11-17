ef7f0520f59df8b2b28d7a7356f9a2bff0c2ee90
Arthur Cougé <arthur.couge@gmail.com>
2018-11-17 14:48:43 +0100

    Add of IA scripts and game object

---
+++ Assets/IA.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour {
+
+	float speed = 2.5f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2 (speed, 0);
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "triggerIA") {
+			rb.velocity = new Vector2 (0, 0);
+			rb.AddForce (new Vector2 (0, 200), ForceMode2D.Impulse);
+		}
+	}
+}
